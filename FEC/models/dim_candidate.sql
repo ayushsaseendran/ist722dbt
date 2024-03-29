e1223e616ba0f68aa5f770bf907d8ca7188a2b4d
Ayush Thoniparambil Saseendran <athonipa@g.syr.edu>
2024-03-29 05:38:23 +0000

    mycommit

--- FEC/models/dim_candidate.sql
+++ FEC/models/dim_candidate.sql
@@ -2,39 +2,49 @@
 
 WITH source_candidates AS (
   SELECT
-    CAND_ID,
-    PRINCIPAL_CMTE_ID,
-    CAND_PTY_AFFILIATION,
-    CAND_OFFICE,
-    CAND_ELECTION_YR,
-    CAND_OFFICE_ST,
-    CAND_OFFICE_DISTRICT,
-    CAND_ICI,
-    CAND_STATUS,
-    CAND_NAME,
-    CAND_ST1,
-    CAND_ST2,
-    CAND_CITY,
-    CAND_ST,
-    CAND_ZIP
-  FROM {{ source('STAGED', 'STA_CN') }}
+    cn.CAND_ID,
+    cn.CAND_NAME,
+    cn.CAND_PTY_AFFILIATION,
+    cn.CAND_ELECTION_YR,
+    cn.CAND_OFFICE_ST,
+    cn.CAND_OFFICE,
+    cn.CAND_OFFICE_DISTRICT,
+    cn.CAND_ICI,
+    cn.CAND_STATUS,
+    cn.CAND_PCC,
+    cn.CAND_ST1,
+    cn.CAND_ST2,
+    cn.CAND_CITY,
+    cn.CAND_ST,
+    cn.CAND_ZIP,
+    cm.CMTE_ID AS PRINCIPAL_CMTE_ID
+  FROM {{ source('STAGED', 'STA_CN') }} cn
+  LEFT JOIN {{ source('STAGED', 'STA_CM') }} cm ON cn.CAND_PCC = cm.CMTE_ID
+),
+
+candidate_keys AS (
+  SELECT
+    ROW_NUMBER() OVER (ORDER BY CAND_ID) AS candidate_key,
+    CAND_ID
+  FROM source_candidates
 )
 
 SELECT
-  ROW_NUMBER() OVER (ORDER BY CAND_ID) AS candidate_key,  -- Generates a surrogate key for candidate_key
-  CAND_ID AS candidate_id,
-  PRINCIPAL_CMTE_ID AS principal_committee_id,
-  CAND_PTY_AFFILIATION AS party_code,
-  CAND_OFFICE AS office_code,
-  CAND_ELECTION_YR::INT AS election_year,
-  CAND_OFFICE_ST AS office_state_code,
-  CAND_OFFICE_DISTRICT AS district_number,
-  CAND_ICI AS incumbent_code,
-  CAND_STATUS AS status_code,
-  CAND_NAME AS candidate_name,
-  CAND_ST1 AS street_address1,
-  CAND_ST2 AS street_address2,
-  CAND_CITY AS city,
-  CAND_ST AS state_code,
-  CAND_ZIP AS zip_code
-FROM source_candidates
+  ck.candidate_key,
+  sc.CAND_ID AS candidate_id,
+  sc.PRINCIPAL_CMTE_ID,
+  sc.CAND_PTY_AFFILIATION AS party_code,
+  sc.CAND_OFFICE AS office_code,
+  sc.CAND_ELECTION_YR::INT AS election_year,
+  sc.CAND_OFFICE_ST AS office_state_code,
+  sc.CAND_OFFICE_DISTRICT AS district_number,
+  sc.CAND_ICI AS incumbent_code,
+  sc.CAND_STATUS AS status_code,
+  sc.CAND_NAME AS candidate_name,
+  sc.CAND_ST1 AS street_address1,
+  sc.CAND_ST2 AS street_address2,
+  sc.CAND_CITY AS city,
+  sc.CAND_ST AS state_code,
+  sc.CAND_ZIP AS zip_code
+FROM source_candidates sc
+JOIN candidate_keys ck ON sc.CAND_ID = ck.CAND_ID
