66d42802620fdbc0f94462b49cbe8fae394cfad8
Ayush Thoniparambil Saseendran <athonipa@g.syr.edu>
2024-04-04 01:05:29 +0000

    mycommit

--- FEC/models/dim_contributors.sql
+++ FEC/models/dim_contributors.sql
@@ -5,15 +5,22 @@ with
             name as contributor_name,
             city,
             state,
-            zip_code as zip_code,
+            zip_code,
             employer,
-            occupation
+            occupation,
+            TRANSACTION_TP as transaction_type,
+            TRANSACTION_AMT as transaction_amount,
+            TRAN_ID as transaction_id
         from {{ source("STAGED", "STA_INDIV") }}
         where
             name is not null
             and city is not null
             and state is not null
             and zip_code is not null
+            -- Assuming you might also want to filter out null transactions, if not remove this line
+            and TRANSACTION_TP is not null
+            and TRANSACTION_AMT is not null
+            and TRAN_ID is not null
     )
 
 select
@@ -21,9 +28,12 @@ select
         order by contributor_name, city, state, zip_code
     ) as contributor_key,
     contributor_name,
-    city as city,
-    state as state,
+    city,
+    state,
     zip_code,
-    employer as employer,
-    occupation as occupation
+    employer,
+    occupation,
+    transaction_type,
+    transaction_amount,
+    transaction_id
 from contributor_data
